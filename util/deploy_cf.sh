ad3323fea0134d4ac544f1c3bda860624d725bff
Andrew Gold <andrewgold@google.com>
2022-01-14 15:43:06 +0000

    Added a util directory with samples.
    deploy_cf.sh - a sample Cloud Function deployment.
    server.py - a sample local web server with CORS support.

---
+++ util/deploy_cf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+################################################################################
+# Copyright Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+# You can usually change the runtime.
+# Note the UNSAFE "--allow-unauthenticated".
+# Note the "--region" argument. 
+gcloud functions deploy analyzeDocument  --runtime nodejs16 --trigger-http --allow-unauthenticated --region us-central1  --project REPLACE_YOUR_PROJECT_NAME
